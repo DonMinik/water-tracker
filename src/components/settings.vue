e859525395e6735fd9a495b0306b3d0027f00efe
DonMinik <D.Mathar@t-online.de>
2022-12-02 12:01:32 +0100

    adds goal tracking

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,12 +1,14 @@
 <template>
 <h1>Settings</h1>
 <v-text-field type='number' label="Size of your cup name" v-model.number='size'></v-text-field>
+<v-text-field type='number' label="Your goal" v-model.number='goal'></v-text-field>
 </template>
 
 <script setup lang='ts'>
 import useLocalStorage from '@/utils/useLocalStorage'
 
 let size = useLocalStorage('size', 200)
+let goal = useLocalStorage('goal', 2000)
 </script>
 <style>
 h1 {
--- src/components/water-tracking.vue
+++ src/components/water-tracking.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
-
-     
-     
       <p>Track how much you drank today!</p>
-         <h2 >You drank <b>{{waterOfToday()}} ml</b> today <br>({{date.toLocaleDateString()}}).</h2>
-   
+     
+      <h2 >You drank <b>{{waterOfToday()}} ml</b> of your goal {{goal}} ml today ({{date.toLocaleDateString()}}).</h2>
+
+      <v-progress-linear class="progress" :color="goalAchievement === 100 ? 'accent' :'primary'" v-model="goalAchievement" :height="12"></v-progress-linear>
+      
       <v-btn   color="primary" variant="outlined" class='drinkBtn'
                elevation="5" @click="drink">
         <v-icon elevation='14' @click="drink"  style="font-size:7rem;" color='accent' >mdi-cup-water
@@ -25,6 +25,9 @@ import {HistoryEntry} from '@/utils/model'
 let date = today()
 let history = useLocalStorage('history', [{date: date.toDateString(), water: 0 }])
 const size = useLocalStorage('size', 200)
+const goal = useLocalStorage('goal', 2000)
+let goalAchievement = 0;
+goalAchievement = calculateGoalAchievement()
 
 function waterOfToday(): HistoryEntry {
   return history.value.find((entry: HistoryEntry) => entry.date === date.toDateString())?.water;
@@ -36,6 +39,12 @@ function drink() {
   let entry = updatedHistory.find((entry: HistoryEntry) => entry.date === date.toDateString());
   entry.water += size.value;
   history.value = updatedHistory
+  goalAchievement = calculateGoalAchievement()
+}
+
+function calculateGoalAchievement(): number {
+  const _goalAchievement = waterOfToday() / goal.value * 100
+  return _goalAchievement > 100 ? 100 : _goalAchievement;
 }
 
 function updateDate() {
@@ -60,6 +69,10 @@ h3, p {
 
 .drinkBtn {
  height: 8rem !important;
+}
+
+.progress {
+  margin: 1rem
 }
 
 </style>
